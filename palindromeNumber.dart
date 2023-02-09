8bc6528123a0fef5eeac4846d634d1b69956e26a
Igor Anthony <95139269+igorAnthony@users.noreply.github.com>
2023-02-09 15:50:44 -0300

    Add files via upload

--- palindromeNumber.dart
+++ palindromeNumber.dart
@@ -4,7 +4,22 @@ class Solution {
     return stringNumber == stringNumber.split('').reversed.join();
   }
 }
+class Solution2 {
+  bool isPalindrome(int x) {
+    double aux;
+    int inputNumber = x;
+    int newNumber = 0;
+    while(x > 0){
+      print("valor antigo: $newNumber");
+      newNumber = newNumber*10 + x%10;print("valor de novo: $newNumber");
+      aux = x/10; 
+      x = aux.toInt();
+      print("valor de x: $x");
+    }
+    return inputNumber == newNumber;
+  }
+}
 void main(){
-  Solution solution = Solution();
+  Solution2 solution = Solution2();
   print(solution.isPalindrome(545));
 }
