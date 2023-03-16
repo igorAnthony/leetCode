1e6d5d13f4ae8a2c3364f54fee2583f0444f7950
Igor Anthony <igor.anthony@outlook.com>
2023-03-16 10:25:16 -0300

    solution 290

--- wordPattern.dart
+++ wordPattern.dart
@@ -1,26 +1,21 @@
 class Solution {
   bool wordPattern(String pattern, String s) {
-    List<String> list = s.split(" ");
-    List<String> list2 = pattern.split("");
-    String left = list2[0];
-    String right = list2[list2.length - 1];
-    String right2 = list[list.length - 1][0];
-    int right_index = list.length - 1;
-    for (int i = 0; i < list.length;i++) {
-      left = list2[i];
-      right = list2[right_index];
-      right2 = list[right_index][0];
-      if(left == right){
-        if(list[i][0] != right2) return false;
-      }else{
-        if(list[i][0] == right2) return false;
-      }
-      right_index--;
+    List<String> words = s.split(" ");
+    if(words.length != pattern.length) return false;
+    
+    Map<String, String> wordPatternMap = {};
+    
+    for(int i = 0; i < words.length; i++){
+      if(!wordPatternMap.containsKey(pattern[i]) && !wordPatternMap.containsValue(words[i])){
+        wordPatternMap[pattern[i]] = words[i];
+      }else if(wordPatternMap[pattern[i]] != words[i]){
+        return false;
+      }    
     }
     return true;
   }
 }
 void main(){
   Solution s = Solution();
-  print(s.wordPattern("aaaa", "dog dat dat dish"));
+  print(s.wordPattern("ab", "happy hacking"));
 }
