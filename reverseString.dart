ad27dd4670ce256394e89bc02839b8391881d952
Igor Anthony <igor.anthony@outlook.com>
2023-03-17 12:19:13 -0300

    344

---
+++ reverseString.dart
@@ -0,0 +1,18 @@
+class Solution {
+  void reverseString(List<String> s) {
+    int j = s.length - 1;
+    String aux = "";
+    for(int i = 0; i < s.length; i++){
+      if(i == j || i > j) break;
+      aux = s[i];
+      s[i] = s[j];
+      s[j] = aux;
+      j--;
+    }
+    print(s);
+  }
+}
+void main(){
+  Solution s = Solution();
+  s.reverseString(["h","e","l","l","o"]);
+}
